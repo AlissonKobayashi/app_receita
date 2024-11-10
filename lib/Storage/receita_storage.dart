0666dbdf22d0d5291c98646520c332e2cd5fcb1c
AlissonKobayashi <134564328+AlissonKobayashi@users.noreply.github.com>
2024-11-09 22:33:06 -0300

    Update receita_storage.dart

--- lib/Storage/receita_storage.dart
+++ lib/Storage/receita_storage.dart
@@ -5,14 +5,12 @@ import 'package:app_receita/models/receita.dart';
 class ReceitaStorage {
   static const String _keyReceitas = "receitas";
 
-  // Salvar receitas
   static Future<void> salvarReceitas(List<Receita> receitas) async {
     final prefs = await SharedPreferences.getInstance();
     List<String> receitasJson = receitas.map((receita) => jsonEncode(receita.toJson())).toList();
     await prefs.setStringList(_keyReceitas, receitasJson);
   }
-
-  // Carregar receitas
+  
   static Future<List<Receita>> carregarReceitas() async {
     final prefs = await SharedPreferences.getInstance();
     List<String>? receitasJson = prefs.getStringList(_keyReceitas);
